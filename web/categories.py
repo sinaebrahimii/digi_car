3a861bee434f738d516fa757e9d57dd6be3e6607
sinaEbrahimi <sinaebrahimi31@gmail.com>
2024-08-01 01:15:28 +0330

    CRUD functions created for categories

--- web/categories.py
+++ web/categories.py
@@ -1,4 +1,4 @@
-from fastapi import  APIRouter,Depends,HTTPException
+from fastapi import  APIRouter,Depends,HTTPException,Path
 from database import SessionLocal
 from starlette import status
 from sqlalchemy.orm import Session 
@@ -32,3 +32,22 @@ async def create_category(db:db_dependency,category:str):
     db.commit()
     return ("Item created successfully")
 
+@router.put('/{category_id}',status_code=status.HTTP_201_CREATED)
+async def update_category(db:db_dependency,category_name:str,category_id:int=Path(gt=0)):
+    category=db.query(Category).filter(Category.id==category_id).first()
+    if category is None :
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Category not found")
+    category.name=category_name
+    print(category_name)
+    db.add(category)
+    db.commit()
+    return("Category updated.")
+
+@router.delete('/{category_id}',status_code=status.HTTP_200_OK)
+async def delete_category(db:db_dependency,category_id:int =Path(gt=0)):
+    category=db.query(Category).filter(Category.id==category_id).first()
+    if category is None :
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Category not found")
+    db.query(Category).filter(Category.id==category_id).delete()
+    db.commit()
+    return("Category Deleted.")
