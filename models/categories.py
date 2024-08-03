c941da274ac2c213f07a3910c125d31dd087b596
sinaEbrahimi <sinaebrahimi31@gmail.com>
2024-08-04 02:18:25 +0330

    changed model

--- models/categories.py
+++ models/categories.py
@@ -10,5 +10,5 @@ from database import Base
 class Category(Base):
     __tablename__ = "categories"
 
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, unique=True, index=True)
+    id :Mapped[int]=mapped_column(primary_key=True)
+    name : Mapped[str]
