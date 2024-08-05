7396250fe22491f69cf4063c88a01c3214d6a377
sinaEbrahimi <sinaebrahimi31@gmail.com>
2024-08-05 19:00:58 +0330

    cretaed password check and improved the  user model

--- models/users.py
+++ models/users.py
@@ -8,9 +8,9 @@ class User(Base):
     __tablename__ = "users"
     
     id:Mapped[int]=mapped_column(primary_key=True)
-    email:Mapped[str]
+    email:Mapped[str]=mapped_column(unique=True)
     password:Mapped[str]
-    role:Mapped[str]
-    first_name:Mapped[str]
-    last_name:Mapped[str]
-    phone_number:Mapped[str]
+    role:Mapped[str]=mapped_column(default="user")
+    first_name:Mapped[str]=mapped_column(nullable=True)
+    last_name:Mapped[str]=mapped_column(nullable=True)
+    phone_number:Mapped[str]=mapped_column(nullable=True)
--- schemas/users.py
+++ schemas/users.py
@@ -2,8 +2,8 @@ from pydantic import BaseModel,EmailStr,Field
 
 class UserCreateRequest(BaseModel):
     email:EmailStr
-    password:str
-    role:str
+    password:str=Field(min_length=8)
+    role:str|None=None
     first_name:str|None=None
     last_name:str|None=None
     phone_number:str|None=None
