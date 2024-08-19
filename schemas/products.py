e3de469662a60915321b630707c43ae6710c04d4
sinaEbrahimi <sinaebrahimi31@gmail.com>
2024-08-19 13:34:52 +0330

    CRUD completed for products

--- schemas/products.py
+++ schemas/products.py
@@ -1,12 +1,20 @@
-from pydantic import BaseModel
+from pydantic import BaseModel,ConfigDict
 
 class ProductRequest(BaseModel):
+    name:str
+    description:str="unique product"
+    price:int
+    stock_quantity:int=0
+    category_id:int|None=None
+
+class ProductResponse(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+    id:int
     name:str
     description:str
     price:int
     stock_quantity:int
     category_id:int|None=None
-
 
 class CategoryRequest(BaseModel):
     name:str
--- web/products.py
+++ web/products.py
@@ -1,8 +1,8 @@
-from fastapi import  APIRouter,Depends
+from fastapi import  APIRouter,Depends,Path,HTTPException
 from database import SessionLocal
 from sqlalchemy.orm import Session 
 from typing import Annotated
-from schemas.products import ProductRequest
+from schemas.products import ProductRequest,ProductResponse
 from models.products import Product
 
 def get_db():
@@ -21,15 +21,54 @@ router = APIRouter(prefix="/products",tags=["products"])
 async def get_all_products(db:db_dependency):
     products=db.query(Product).all()
     return products
+
+@router.get("/{product_id}",response_model=ProductResponse)
+async def get_product(db: db_dependency,product_id: int=Path(gt=0)):
+    product = db.query(Product).filter(Product.id == product_id).first()
     
+    if product is None:
+        raise HTTPException(status_code=404, detail="Product not found")
+    
+    return product
 
 
 
 @router.post("/",status_code=200)
 async def create_procuct(db:db_dependency,product:ProductRequest):
     dict=product.model_dump()
-    product_model=Product(**product.model_dump())
+    product_model=Product(**dict)
     db.add(product_model)
     db.commit()
     print(dict)
-    return (dict)
+    return (product.model_dump())
+
+@router.put("/{product_id}")
+async def update_product(db:db_dependency,product_id: int, product_request: ProductRequest, ):
+    product = db.query(Product).filter(Product.id == product_id).first()
+    if product is None:
+        raise HTTPException(status_code=404, detail="Product not found")
+    
+    product.name = product_request.name
+    product.description = product_request.description
+    product.price = product_request.price
+    product.stock_quantity = product_request.stock_quantity
+    
+    if product_request.category_id is not None:
+        product.category_id = product_request.category_id
+    
+    db.commit()
+    
+    db.refresh(product)
+    
+    return product
+
+@router.delete("/{product_id}")
+async def delete_product( db: db_dependency,product_id: int):
+    product = db.query(Product).filter(Product.id == product_id).first()
+    
+    if product is None:
+        raise HTTPException(status_code=404, detail="Product not found")
+    
+    db.delete(product)
+    db.commit()
+    return {"detail": "Product deleted successfully"}
